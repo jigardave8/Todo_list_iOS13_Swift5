3a2b56d44a61c62ab217e6ac168c60fc2ff0b6c2
DAVEJ2 <iknowed1@gmail.com>
2020-08-19 14:29:38 +0530

    Initial Commit

---
+++ Todo_list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo_list
+//
+//  Created by jigar on 19/08/20.
+//  Copyright © 2020 jigar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
