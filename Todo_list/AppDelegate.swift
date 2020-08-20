266eb3181f4922d64b6eae70146fd4f663f1dda9
DAVEJ2 <iknowed1@gmail.com>
2020-08-20 14:56:29 +0530

    Basic functionality

--- Todo_list/AppDelegate.swift
+++ Todo_list/AppDelegate.swift
@@ -16,9 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("didFinishLaunchingWithOptions")
         return true
     }
 
+    
+    func applicationWillResignActive(_ application: UIApplication){
+        
+        
+    }
+    
+    
+    
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- Todo_list/TodoListViewController.swift
+++ Todo_list/TodoListViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
-   let itemArray = ["Find Jigar", "Buy Jam", " Ketup"]
+    
+    var itemArray = ["Find Jigar", "Buy Jam", " Ketup"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +27,8 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
@@ -37,6 +37,65 @@ class TodoListViewController: UITableViewController {
         
     }
     
-
+    //MARK: -  TableView Delegate Method
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //   print(itemArray[indexPath.row])
+        
+        
+        
+        if
+            tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    //MARK: -  Add New Items
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            // whatever will happen once the user clicks the add item button on our UIAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Item"
+            
+            textField = alertTextField
+            
+         
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
